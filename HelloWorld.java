f6677711f34eec63957fab92ab7e814fb505c5cb
Andres Rodriguez <kflex25@MacBook-Pro-de-Andres.local>
2021-10-19 10:01:37 -0600

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My Name is Andres Rodríguez");
+        System.out.println("I am 19 years old");
+        System.out.println("My hometown is Alajuela");
+    }
+}
